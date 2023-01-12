a86e083be35bcf8a5754fafc2f1dced998c989f2
R.I.Pienaar <rip@devco.net>
2023-01-12 12:31:47 +0100

    (misc) add tools dependencies
    Signed-off-by: R.I.Pienaar <rip@devco.net>

--- standard_test.go
+++ standard_test.go
@@ -1,8 +1,6 @@
-/*
- * Copyright (c) 2022-2023, R.I. Pienaar and the Choria Project contributors
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright (c) 2022-2023, R.I. Pienaar and the Choria Project contributors
+//
+// SPDX-License-Identifier: Apache-2.0
 
 package tokens
 
